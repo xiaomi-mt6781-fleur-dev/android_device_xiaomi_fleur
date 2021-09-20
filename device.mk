eaa6343ee69a231b677a029126ff4ea4b4a7d729
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    fleur: sensors: Avoid target name conflicts
    Change-Id: I170c8c7e4edbfcdebf5d6f6df148b72993d805f6

--- device.mk
+++ device.mk
@@ -399,11 +399,13 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.0-service.multihal \
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
+    android.hardware.sensors@2.1-service.fleur-multihal \
     android.hardware.sensors@1.0.vendor \
     android.hardware.sensors@2.0.vendor \
-    android.hardware.sensors@2.0-ScopedWakelock.vendor \
-    android.frameworks.sensorservice@1.0
+    android.hardware.sensors@2.1.vendor \
+    android.hardware.sensors@2.0-ScopedWakelock.vendor
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge
