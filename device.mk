7b479dfd4a2c3a3e288f46235dea8e183215545b
Blukberry <15341881-blukberry@users.noreply.gitlab.com>
2023-09-10 12:07:01 +0000

    fleur: Define KERNEL_PATH in device.mk

--- device.mk
+++ device.mk
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+KERNEL_PATH := kernel/xiaomi/fleur-prebuilt
+
 # Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
