efec7e7e15d9873a3e8e5726b4725c22e34681e1
Blukberry <15341881-blukberry@users.noreply.gitlab.com>
2023-09-10 12:13:35 +0000

    fleur: Drop VNDK Links

--- device.mk
+++ device.mk
@@ -424,12 +424,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.fleur
 
-# VNDK
-PRODUCT_PACKAGES += \
-    libutils-v32 \
-    libhidlbase-v32 \
-    libui-v32
-
 # Wi-Fi
 PRODUCT_PACKAGES += \
     android.hardware.wifi.supplicant@1.0.vendor \
