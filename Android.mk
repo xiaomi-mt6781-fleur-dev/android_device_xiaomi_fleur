a36cf540a1a0fe4e2b956a9aa07dbe457aecdfe5
Blukberry <15341881-blukberry@users.noreply.gitlab.com>
2023-09-10 12:23:25 +0000

    fleur: Clean up Android.mk from symlinks

--- Android.mk
+++ Android.mk
@@ -6,25 +6,6 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter fleur, $(TARGET_DEVICE)),)
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
-VENDOR_SYMLINKS := \
-    $(TARGET_OUT_VENDOR)/lib \
-    $(TARGET_OUT_VENDOR)/lib64 \
-    $(TARGET_OUT_VENDOR)/lib/hw \
-    $(TARGET_OUT_VENDOR)/lib64/hw
-
-$(VENDOR_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	$(hide) echo "Making vendor symlinks"
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib/hw
-	@mkdir -p $(TARGET_OUT_VENDOR)/lib64/hw
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib/hw/gatekeeper.default.so
-	@ln -sf libSoftGatekeeper.so $(TARGET_OUT_VENDOR)/lib64/hw/gatekeeper.default.so
-	@ln -sf /vendor/lib/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	@ln -sf /vendor/lib64/egl/libGLES_mali.so $(TARGET_OUT_VENDOR)/lib64/hw/vulkan.$(TARGET_BOARD_PLATFORM).so
-	$(hide) touch $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(VENDOR_SYMLINKS)
-
+ifeq ($(TARGET_DEVICE),fleur)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
