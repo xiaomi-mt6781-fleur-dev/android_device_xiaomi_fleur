2c0203cd9ba42486f4601c174be4ac483fe8ce0e
Gabriel Biren <gbiren@google.com>
2023-10-29 16:42:03 +0000

    fleur: Switch to default AIDL WiFi service
    Bug: 274964641
    Test: Pre-submit tests
    Change-Id: I4d9114377fe75b593bbfe6a59f03d8f5d607d6a1
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -436,7 +436,7 @@ PRODUCT_PACKAGES += \
 
 # Wi-Fi
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy.fleur \
+    android.hardware.wifi-service \
     hostapd \
     wpa_supplicant
 
