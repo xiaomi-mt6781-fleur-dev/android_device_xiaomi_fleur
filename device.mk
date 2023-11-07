3ec8437d0d2c14c64cb6843669b71eb48f74bc56
LuK1337 <priv.luk@gmail.com>
2023-11-07 16:27:26 +0100

    fleur: Build libprotobuf-cpp-lite-3.9.1-vendorcompat
    Change-Id: I0aac742db9c2b4b3e48ee20158f19405863893c2

--- device.mk
+++ device.mk
@@ -107,6 +107,7 @@ PRODUCT_COPY_FILES += \
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
+    libprotobuf-cpp-lite-3.9.1-vendorcompat
 
 PRODUCT_PACKAGES += \
     libmockdrmcryptoplugin
