23991e8253ff802edb4fd8b7e1d6a9c9e54eb54d
Signed-off-by: bengris32 <bengris32@protonmail.ch>
2023-12-21 15:03:16 +0000

    fleur: libinit: Set `ro.product.mod_device` property
    Signed-off-by: bengris32 <bengris32@protonmail.ch>

--- libinit/libinit_variant.cpp
+++ libinit/libinit_variant.cpp
@@ -37,6 +37,8 @@ void set_variant_props(const variant_info_t variant) {
     set_ro_build_prop("marketname", variant.marketname, true);
     set_ro_build_prop("model", variant.model, true);
     set_ro_build_prop("mod_device", variant.model, true);
+
+    property_override("ro.product.mod_device", variant.mod_device, true);
     property_override("vendor.usb.product_string", variant.marketname, true);
 
     if (access("/system/bin/recovery", F_OK) != 0) {
