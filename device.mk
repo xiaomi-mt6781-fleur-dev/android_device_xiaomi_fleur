9c46f6861eef42bf90448f78a1a459c9139fbfc4
bengris32 <bengris32@protonmail.ch>
2024-08-20 10:18:50 +0000

    fleur: Switch Boot HAL to AIDL

--- device.mk
+++ device.mk
@@ -14,9 +14,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.2-impl \
-    android.hardware.boot@1.2-impl.recovery \
-    android.hardware.boot@1.2-service
+    android.hardware.boot-service.default \
+    android.hardware.boot-service.default_recovery
 
 PRODUCT_PACKAGES += \
     update_engine \
