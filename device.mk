580be6cdb3158ee0d6e2549dd47b5acd1e1e0a24
Nem1x <nemanjanedeljkovic738@gmail.com>
2024-08-20 11:02:15 +0000

    fleur: Enable updating of APEXes
    * For QPR1

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # A/B
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.default \
