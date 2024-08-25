80ef2456984f225eb9dcbce0cf03f034f47acb93
Nem1x <nemanjanedeljkovic738@gmail.com>
2024-08-25 10:14:22 +0000

    Revert "fleur: Switch to Pixel thermal service"
    This reverts commit 0566e9f74c5ed2039e34b9b90142a5915f5474c9.

--- device.mk
+++ device.mk
@@ -457,10 +457,10 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Thermal
 PRODUCT_PACKAGES += \
-    android.hardware.thermal-service.mediatek
+    android.hardware.thermal@1.0-impl
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal_info_config.json:$(TARGET_COPY_OUT_VENDOR)/etc/thermal_info_config.json
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0.vendor
 
 # USB
 PRODUCT_PACKAGES += \
